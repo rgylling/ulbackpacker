1292b737b1daaf00737e5e6685e0dbce4cfadd41
Gylling, Randy <Randy.Gylling@rexelusa.com>
2019-05-28 14:49:59 -0700

    converted to nuxt for SSR and better SEO

---
+++ src/middleware/test.js
@@ -0,0 +1,8 @@
+
+export default function ({ app }) {
+  // if (app.$fireAuth) {
+  //   console.log(
+  //     'Firebase Services can be accessed with app.$fireAuth (etc.) in Middleware.'
+  //   )
+  // }
+}
---
+++ src/pages/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-text-field v-model="email" label="email" />
+    <v-text-field v-model="password" label="password" />
+    <v-btn @click="createUser">
+      create user
+    </v-btn>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  mounted() {
+    return console.log(this)
+  },
+  methods: {
+    async createUser() {
+      try {
+        await this.$fireAuth.createUserWithEmailAndPassword(
+          this.email,
+          this.password
+        )
+      } catch (e) {
+        alert(e)
+      }
+    }
+  }
+}
+</script>
